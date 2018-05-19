6f5732d3fc2c9596c62c7e991b77f1eef0d85ed0
Waleed Jaffar <waleedjaffar@gmail.com>
2018-05-20 01:06:31 +0530

    Added validations and short_url_generator

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,19 @@
 class Link < ActiveRecord::Base
+
+  before_validation :generate_short_url
+  validates :full_url, :short_url, presence: true
+  validates :full_url, url: true
+
+  private
+
+  # Generate a unique short_url for a given web address
+  def generate_short_url
+    url = Process.clock_gettime(Process::CLOCK_REALTIME, :nanosecond).to_s(36)
+    old_url = Link.where(short_url: url).last
+    if old_url.present? 
+      self.generate_short_url
+    else
+      self.short_url = url
+    end
+  end
 end
--- db/migrate/20180519180526_create_links.rb
+++ db/migrate/20180519180526_create_links.rb
@@ -1,9 +1,9 @@
 class CreateLinks < ActiveRecord::Migration
   def change
     create_table :links do |t|
-      t.string :full_url
-      t.string :short_url
-      t.integer :access_count
+      t.string :full_url, null: false
+      t.string :short_url, null: false
+      t.integer :access_count, null: false, default: 0
 
       t.timestamps null: false
     end
